a1dde647ecd9d4380f85535cb01bdb4731c24668
Luka Cabraja <luka@cabraja.eu>
2022-01-18 20:49:59 +0100

    init commit

---
+++ Memorize/Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by doss on 1/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by doss on 1/18/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
